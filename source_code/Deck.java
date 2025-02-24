64785fc8fdc2d9152829f10dece7902599fd3b80
ChelsieGonzales <chelseeyaaaa@gmail.com>
2025-02-24 10:16:50 +0800

    Create Deck.java

---
+++ source_code/Deck.java
@@ -0,0 +1,30 @@
+import java.util.ArrayList;
+import java.util.Collections;
+
+class Deck {
+    private ArrayList<Card> cards;
+
+    public Deck() {
+        cards = new ArrayList<>();
+        for (Suit suit : Suit.values()) {
+            for (Rank rank : Rank.values()) {
+                cards.add(new Card(suit, rank));
+            }
+        }
+    }
+
+    public void shuffle() {
+        Collections.shuffle(cards);
+    }
+
+    public Card dealCard() {
+        if (!cards.isEmpty()) {
+            return cards.remove(0);
+        }
+        return null;
+    }
+
+    public boolean isEmpty() {
+        return cards.isEmpty();
+    }
+}
